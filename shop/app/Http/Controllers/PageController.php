f0cd6908894fe6f2a67865eb1bfedfe621ad1bb9
hotshot12266 <kaka12266@gmail.com>
2019-03-17 22:26:28 +0700

    shop.dathang

---
+++ shop/app/Http/Controllers/PageController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Slide;
+use App\Product;
+use App\ProductType;
+use App\Cart;
+use Session;
+use App\Customer;
+use App\Bill;
+use App\BillDetail;
+
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function getIndex(){
+        $slide=Slide::all();
+    	// return view('page.trangchu',['slide'=>$slide]);/// cach 1 chuyen du lieu vao slide 
+        $new_product=Product::where('new',1)->paginate(4);
+        $sanpham_khuyenmai=Product::where('promotion_price','<>',0)->paginate(8);
+        // dd($new_product);
+        return view('page.trangchu',compact('slide','new_product','sanpham_khuyenmai'));
+    }
+
+    public function getLoaiSp($type){
+        $sp_theoloai=Product::where('id_type',$type)->get();
+        $sp_khac=Product::where('id_type','<>',$type)->paginate(3);
+        $loai=ProductType::all();
+        $loap_sp=ProductType::where('id',$type)->first();
+        return view('page.loai_sanpham',compact('sp_theoloai','sp_khac','loai','loap_sp'));
+    }
+
+    public function getChitiet($id){
+        $new_product=Product::where('new',1)->paginate(4);
+        $sanpham= Product::where('id',$id)->first();
+        $sp_tuongtu= Product::where('id_type',$sanpham->id_type)->paginate(6);
+        return view('page.chitiet_sanpham',compact('sanpham','sp_tuongtu','new_product'));
+    }
+
+    public function getLienHe(){
+    	return view('page.lienhe');
+    }
+
+    public function getGioiThieu(){
+    	return view('page.gioithieu');
+    }
+
+    public function getAddtoCart(Request $req,$id){
+        $product=Product::find($id);
+        $oldCart=Session('cart')?Session::get('cart'):null;
+        $cart=new Cart($oldCart);
+        $cart->add($product,$id);
+        $req->session()->put('cart',$cart);
+        return redirect()->back();
+    }
+
+    public function getDelItemCart($id){
+
+        $oldCart=Session('cart')?Session::get('cart'):null;
+        $cart=new Cart($oldCart);
+        $cart->removeItem($id);
+        if(count($cart->items)>0){
+            Session::put('cart',$cart);
+        }
+        else{
+            Session::forget('cart');
+        }
+        return redirect()->back();
+    }
+
+    public function getCheckout(){
+        if(Session::has('cart')){
+            $oldCart=Session::get('cart');
+            $cart=new Cart($oldCart);
+            return view('page.dat_hang',['product_cart'=>$cart->items,'totalPrice'=>$cart->totalPrice,'totalQty'=>$cart->totalQty]);
+        }
+        else{
+            return view('page.dat_hang');
+        }
+    }
+
+    public function postCheckout(Request $req){
+        $cart=Session::get('cart');
+
+        $customer=new Customer;
+        $customer->name=$req->full_name;
+        $customer->gender=$req->gender;
+        $customer->email=$req->email;
+        $customer->address=$req->address;
+        $customer->phone_number=$req->phone;
+        $customer->note=$req->notes;
+        $customer->save();   
+
+        $bill=new Bill;
+        $bill->id_customer=$customer->id;
+        $bill->date_order=date('Y-m-d');
+        $bill->total=$cart->totalPrice;
+        $bill->payment=$req->payment_method;
+        $bill->note=$req->notes;
+        $bill->save();  
+
+        foreach($cart->items as $key=>$value){
+            $bill_detail=new BillDetail;
+            $bill_detail->id_bill=$bill->id;
+            $bill_detail->id_product=$key;
+            $bill_detail->quantity=$value['qty'];
+            $bill_detail->unit_price=$value['price']/$value['qty'];
+            $bill_detail->save();  
+        }
+        Session::forget('cart');
+        return redirect()->back()->with('thongbao','Đặt hàng thành công');
+    }
+}
